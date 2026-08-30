9462457b5e9dc4dec60badda1c77197e15909142
TIO KRISNA <tiokrisna05@gmail.com>
2026-08-30 11:49:02 +0800

    Fix Vercel asset base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/mepandes-alya-dan-deya/',
+    base: '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
