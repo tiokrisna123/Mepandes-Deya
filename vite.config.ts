e51aceaf9fc7bbe5cbdc9d27cd64d04930b6c35e
TIO KRISNA <tiokrisna05@gmail.com>
2026-08-30 11:40:42 +0800

    Fix Deya Vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-     base: '/mepandes-alya-dan-deya/',
+    base: '/mepandes-alya-dan-deya/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
